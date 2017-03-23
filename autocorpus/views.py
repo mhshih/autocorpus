d80d3714c0bb21b872cc8cada13aecb32b2ff544
mhshih <d00142002@ntu.edu.tw>
2017-03-23 20:04:10 +0800

    F.writed and parse(F) then Generate new file radio

--- autocorpus/views.py
+++ autocorpus/views.py
@@ -9,9 +9,9 @@ def append2DRH(dep,rel,head,DRH):
     else:DRH[dep][rel].append(head)
 
 from os import chdir,system
-def parse(file='chinese-onesent-utf8.txt'):
+def parse(filename='chinese-onesent-utf8.txt'):
     chdir('/tmp/stanford-parser-full-2016-10-31')
-    system('bash lexparser.sh data/'+file+' > lexparser.parsed')
+    system('bash lexparser.sh /tmp/autocorpus/'+filename+' > lexparser.parsed')
     DRH=dict() #DRH[dep]={rel:heads}
     for line in open('lexparser.parsed').readlines()[:-1]:
         rel,dep_head=line.split('(')[:2]
@@ -25,7 +25,12 @@ def home(request):
     if request.method=='GET':
         return render(request,'template.htm',{'form':UploadFileForm()})
     elif request.method=='POST':
-        DRH=parse(file=request.FILES['file'].name)
+        f=request.FILES['file']
+        t=open(f.name,'wb+')
+        for chunk in f.chunks():
+            t.write(chunk)
+        t.close()
+        DRH=parse(filename=f.name)
         output='<table>'
         for dep,rel_heads in DRH.items():
             output+='<tr><td>%s</td></tr>' % dep
