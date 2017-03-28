7ee852269afcb2e43dc8b5ef86e8a5ca2a42beb5
mhshih <d00142002@ntu.edu.tw>
2017-03-28 16:32:25 +0800

    Tested english_onesent.txt then Test chinese_onesent_utf8.txt

--- autocorpus/views.py
+++ autocorpus/views.py
@@ -2,45 +2,31 @@ from django import forms
 class UploadFileForm(forms.Form):
     file=forms.FileField()
 
-def append2DRH(dep,rel,head,DRH):
-    if dep not in DRH:DRH[dep]={rel:[head]}
-    elif rel not in DRH[dep]:DRH[dep][rel]=[head]
-    else:DRH[dep][rel].append(head)
-
-from subprocess import call
-from os import chdir,system,listdir
-def parse(filename='chinese-onesent-utf8.txt'):
-    DRH=dict() #DRH[dep]={rel:heads}
-    command='cd /home/simon/stanford-parser-full-2016-10-31; java -mx150m -cp "*:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -outputFormat "typedDependencies" edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz data/english-onesent.txt > /tmp/autocorpus/english-onesent.txt.out'
-    print(command)
-    system(command)
-    for line in open('/tmp/autocorpus/english-onesent.txt.out').readlines()[:-1]:
-        rel,dep_head=line.split('(')[:2]
-        dep,head=[w.split('-')[0] for w in dep_head.split()]
-        append2DRH(dep,rel,head,DRH)
-    return DRH
-
+from parse import read_parse
 from django.http import HttpResponse
+from django.shortcuts import render
 def query(request):
     corpus=request.POST['radio']
-    return HttpResponse(corpus)
+    DRH=read_parse(corpus)
+    dependent=request.POST['word']
+#   return HttpResponse(DRH[dependent].values()[0])
+    return render(request,'collocation.htm',{'RH':DRH[dependent]})
 
+def write_RF(RF):
+    f=open(RF.name,'wb+')
+    for chunk in RF.chunks():f.write(chunk)
+    f.close()
+
+from os import listdir
 from django.shortcuts import render,redirect
+from parse import parse
 def home(request):
     corpora=[fn for fn in listdir('.') if fn.endswith('out')]
     if request.method=='GET':
         return render(request,'template.htm',{'corpora':corpora,'upload_form':UploadFileForm()})
     elif request.method=='POST':
-#       f=request.FILES['file']
-#       t=open(f.name,'wb+')
-#       for chunk in f.chunks():t.write(chunk)
+        RF=request.FILES['file']
+        write_RF(RF)
+        parse(filename=RF.name)
         return redirect('/')
-        return HttpResponse('<input type=radio name=radio value=%s />%s' % (corpora[0],corpora[0]))
 
-        output='<table>'
-        DRH=parse()#filename=f.name)
-        for dep,rel_heads in DRH.items():
-            output+='<tr><td>%s</td></tr>' % dep
-            for rel,heads in rel_heads.items():
-                output+='<tr><td></td><td>%s:</td><td>%s</td><tr>' % (rel,' '.join(heads))
-        return HttpResponse(output)
---
+++ parse.py
@@ -0,0 +1,21 @@
+from os import system
+
+def append2DRH(dep,rel,head,DRH):
+    if dep not in DRH:DRH[dep]={rel:[head]}
+    elif rel not in DRH[dep]:DRH[dep][rel]=[head]
+    else:DRH[dep][rel].append(head)
+
+def parse(filename):#='chinese-onesent-utf8.txt'):
+    command='cd /home/simon/stanford-parser-full-2016-10-31; java -mx150m -cp "*:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -outputFormat "typedDependencies" edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz /tmp/autocorpus/%s > /tmp/autocorpus/%s.out' % (filename,filename)
+#   command='cd /home/simon/stanford-parser-full-2016-10-31; java -mx150m -cp "*:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -outputFormat "typedDependencies" edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz /tmp/autocorpus/english-onesent.txt > /tmp/autocorpus/english-onesent.txt.out'
+    print(command)
+    system(command)
+
+def read_parse(filename):
+    DRH=dict() #DRH[dep]={rel:heads}
+    for line in open('/tmp/autocorpus/%s' % filename).readlines()[:-1]:
+        rel,dep_head=line.split('(')[:2]
+        dep,head=[w.split('-')[0] for w in dep_head.split()]
+        append2DRH(dep,rel,head,DRH)
+    return DRH
+
